cfb1fbf5f28cedfd2d4603b965eb92707d89e375
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2017-12-22 22:48:29 -1000

    Additional shapes
    - Ability to draw lines and gradient shapes (circle or rectangle)
    - Additional methods for existing shapes
    - Updated test class accordingly

--- EGL2.java
+++ EGL2.java
@@ -20,7 +20,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class EGL2 { // No longer extends JPanel. Instead is used to manage frames.
 					// Panel is made to draw shapes on.
 	
-	public static EGL2 egl2;
+	public static Color backgroundColor = new Color(255, 255, 255);
 	public static Color BLACK = new Color(0, 0, 0);
 	public static Color BLUE = new Color(0, 0, 255);
 	
@@ -35,6 +35,8 @@ public class EGL2 { // No longer extends JPanel. Instead is used to manage frame
 	// Need to use CopyOnWriteArrayList to avoid ConcurrentModificationException
 	protected static CopyOnWriteArrayList<Rectangle> rects = new CopyOnWriteArrayList<Rectangle>();
 	protected static CopyOnWriteArrayList<Circle> circles = new CopyOnWriteArrayList<Circle>();
+	protected static CopyOnWriteArrayList<Line> lines = new CopyOnWriteArrayList<Line>();
+	protected static CopyOnWriteArrayList<Gradient> gradients = new CopyOnWriteArrayList<Gradient>();
 
 	
 	private static int FPS;
@@ -427,12 +429,14 @@ public class EGL2 { // No longer extends JPanel. Instead is used to manage frame
 		FPS = fPS;
 	}
 	
-	/*public void paint(Graphics2D g) {
-		//g.setColor(Color.WHITE);
-		//g.fillRect(0, 0, 1000, 1000);
-		repaint();
-	}*/
-
+	/**
+	 * <code>public static void setBackgroundColor(Color color)</code></br></br>
+	 * Sets the background color of the window
+	 * @param color Color to change the background to
+	 */
+	public static void setBackgroundColor(Color color) {
+		backgroundColor = color;
+	}
 	
 	/**
 	 * The Draw class is used for drawing shapes to the
@@ -490,6 +494,47 @@ public class EGL2 { // No longer extends JPanel. Instead is used to manage frame
 			return newCircle;
 		}
 		
+		/**
+		 * <code>public static Line line(int x1, int y1, int x2, int y2, Color color)</code></br></br>
+		 * Can be used to draw a line.
+		 * @param x1 starting x coordinate
+		 * @param x2 ending x coordinate
+		 * @param y1 starting y coordinate
+		 * @param y2 ending y coordinate
+		 * @param color The color to use when drawing the line
+		 */
+		public static Line line(int x1, int y1, int x2, int y2, Color color) {
+			
+			Line newLine = new Line(x1, y1, x2, y2, color);
+			return newLine;
+		}
+		
+		/**
+		 * <code>public static Gradient gradient(int _x, int _y, int _w, int _h, int _x1, int _y1, Color _color1,
+										int _x2, int _y2, Color _color2, boolean _repeat, String _shape)</code></br></br>
+		 * Draws a gradient shape to the screen
+		 * @param _x Coordinate to draw the shape
+		 * @param _y Coordinate to draw the shape
+		 * @param _w Width
+		 * @param _h Height
+		 * @param _x1 first x coordinate
+		 * @param _y1 first y coordinate
+		 * @param _color1 color at the first point
+		 * @param _x2 second x coordinate
+		 * @param _y2 second y coordinate
+		 * @param _color2 color at the second point
+		 * @param _repeat colors will repeat if true, will not repeat if false
+		 * @param _shape String which determines the type of shape that will be drawn.
+		 * 			It must be either "rectangle" or "circle". If it is not one of
+		 * 			these, the shape will not be drawn.
+		 */
+		public static Gradient gradient(int _x, int _y, int _w, int _h, int _x1, int _y1, Color _color1,
+										int _x2, int _y2, Color _color2, boolean _repeat, String _shape) {
+			
+			Gradient newGradient = new Gradient(_x, _y, _w, _h, _x1, _y1, _color1, _x2, _y2, _color2, _repeat, _shape);
+			return newGradient;
+		}
+		
 	}
 	
 }
@@ -506,7 +551,7 @@ public class EGL2 { // No longer extends JPanel. Instead is used to manage frame
  */
 class Circle {
 	
-	boolean filled;
+	boolean filled, visible = true;
 	Color color;
 	int x, y, w, h;
 		
@@ -544,7 +589,68 @@ class Circle {
 	public void setColor(Color c) {
 		color = c;
 	}
+	
+	/**
+	 * <code>public Color getColor()</code></br></br>
+	 * Used to get the current color of the circle
+	 * @return the current color of the circle
+	 */
+	public Color getColor() {
+		return color;
+	}
+	
+	/**
+	 * Moves the circle by the specified amounts
+	 * @param x amount to move on the x axis
+	 * @param y amount to move on the y axis
+	 */
+	public void translate(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	/**
+	 * Will extrude the shape from the center out by the amounts specified in the
+	 * parameters. Can shrink the shape as well using negative parameters.
+	 * @param widthAmount amount to magnify the width by
+	 * @param heightAmount amount to magnify the height by
+	 */
+	public void extrude(int widthAmount, int heightAmount) {
+		// width and height amounts must be multiplied by 2 when adding to w and h
+		// instead of dividing them by 2 when subtracting them from x and y, because
+		// if extrude is called with parameters of 1 and 1, the shape will not
+		// grow from the center outward.
+		w += widthAmount * 2.0;
+		h += heightAmount * 2.0;
+		x -= widthAmount;
+		y -= heightAmount;
+	}
 		
+	/**
+	 * <code>public void show()</code></br></br>
+	 * Make the shape visible
+	 */
+	public void show() {
+		visible = true;
+	}
+	
+	/**
+	 * <code>public void hide()</code></br></br>
+	 * Hide the shape
+	 */
+	public void hide() {
+		visible = false;
+	}
+	
+	/**
+	 *<code>public boolean isVisible()</code></br></br>
+	 * Checks whether the shape is visible or not
+	 * @return true if visible, false if not
+	 */
+	public boolean isVisible() {
+		return visible;
+	}
+	
 }
 
 // So that the user does not need to import java.awt.Rectangle to create
@@ -559,7 +665,7 @@ class Circle {
  */
 class Rectangle {
 	
-	boolean filled;
+	boolean filled, visible = true;
 	Color color;
 	int x, y, w, h;
 	
@@ -596,6 +702,311 @@ class Rectangle {
 		color = c;
 	}
 	
+	/**
+	 * <code>public Color getColor()</code></br></br>
+	 * Used to get the current color of the rectangle
+	 * @return the current color of the rectangle
+	 */
+	public Color getColor() {
+		return color;
+	}
+	
+	/**
+	 * Moves the rectangle by the specified amounts
+	 * @param x amount to move on the x axis
+	 * @param y amount to move on the y axis
+	 */
+	public void translate(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	/**
+	 * Will extrude the shape from the center out by the amounts specified in the
+	 * parameters. Can shrink the shape as well using negative parameters.
+	 * @param widthAmount amount to magnify the width by
+	 * @param heightAmount amount to magnify the height by
+	 */
+	public void extrude(int widthAmount, int heightAmount) {
+		// width and height amounts must be multiplied by 2 when adding to w and h
+		// instead of dividing them by 2 when subtracting them from x and y, because
+		// if extrude is called with parameters of 1 and 1, the shape will not
+		// grow from the center outward.
+		w += widthAmount * 2.0;
+		h += heightAmount * 2.0;
+		x -= widthAmount;
+		y -= heightAmount;
+	}
+	
+	/**
+	 * <code>public void show()</code></br></br>
+	 * Make the shape visible
+	 */
+	public void show() {
+		visible = true;
+	}
+	
+	/**
+	 * <code>public void hide()</code></br></br>
+	 * Hide the shape
+	 */
+	public void hide() {
+		visible = false;
+	}
+	
+	/**
+	 *<code>public boolean isVisible()</code></br></br>
+	 * Checks whether the shape is visible or not
+	 * @return true if visible, false if not
+	 */
+	public boolean isVisible() {
+		return visible;
+	}
+	
+}
+
+/**
+ * Class used to draw new Line objects. The coordinates
+ * of the line are mutable. They can be changed at any time. To move the whole line,
+ * it is easiest to use the translate() method instead of changing each of
+ * the coordinates individually.
+ *
+ */
+class Line {
+	
+	Color color;
+	int x1, x2, y1, y2;
+	boolean visible = true;
+	
+	/**
+	 * Can be used to draw a line. Works the same as EGL2.Draw.Line()
+	 * @param _x1 starting x coordinate
+	 * @param _x2 ending x coordinate
+	 * @param _y1 starting y coordinate
+	 * @param _y2 ending y coordinate
+	 * @param c The color to use when drawing the rectangle
+	 */
+	public Line(int _x1, int _y1, int _x2, int _y2, Color c) {
+		
+		super();
+		x1 = _x1;
+		x2 = _x2;
+		y1 = _y1;
+		y2 = _y2;
+		color = c;
+		
+		// Add to list of lines
+		EGL2.lines.add(this);
+	}
+	
+	/**
+	 * <code>public void setColor(Color c)</code></br></br>
+	 * Changes the color of the rectangle
+	 * @param c - Color to change to
+	 */
+	public void setColor(Color c) {
+		color = c;
+	}
+	
+	/**
+	 * <code>public Color getColor()</code></br></br>
+	 * Used to get the current color of the line
+	 * @return the current color of the line
+	 */
+	public Color getColor() {
+		return color;
+	}
+	
+	/**
+	 * Moves the entire line rather than just a single coordinate
+	 * @param x amount to move on the x axis
+	 * @param y amount to move on the y axis
+	 */
+	public void translate(int x, int y) {
+		x1 += x;
+		x2 += x;
+		y1 += y;
+		y2 += y;
+	}
+	
+	/**
+	 * <code>public void show()</code></br></br>
+	 * Make the shape visible
+	 */
+	public void show() {
+		visible = true;
+	}
+	
+	/**
+	 * <code>public void hide()</code></br></br>
+	 * Hide the shape
+	 */
+	public void hide() {
+		visible = false;
+	}
+	
+	/**
+	 *<code>public boolean isVisible()</code></br></br>
+	 * Checks whether the shape is visible or not
+	 * @return true if visible, false if not
+	 */
+	public boolean isVisible() {
+		return visible;
+	}
+	
+}
+
+/**
+ * Class used to draw gradient rectangles
+ */
+class Gradient {
+	
+	Color color1, color2;
+	int x, y, w, h, x1, x2, y1, y2;
+	boolean repeat, visible = true;
+	String shape;
+	
+	/**
+	 * Can be used in the same way as EGL2.Draw.gradient()
+	 * @param _x Coordinate to draw the shape
+	 * @param _y Coordinate to draw the shape
+	 * @param _w Width
+	 * @param _h Height
+	 * @param _x1 first x coordinate
+	 * @param _y1 first y coordinate
+	 * @param _color1 color at the first point
+	 * @param _x2 second x coordinate
+	 * @param _y2 second y coordinate
+	 * @param _color2 color at the second point
+	 * @param _repeat colors will repeat if true, will not repeat if false
+	 * @param _shape String which determines the type of shape that will be drawn.
+	 * 			It must be either "rectangle" or "circle". If it is not one of
+	 * 			these, the shape will not be drawn.
+	 */
+	public Gradient(int _x, int _y, int _w, int _h, int _x1, int _y1, Color _color1,
+			int _x2, int _y2, Color _color2, boolean _repeat, String _shape) {
+		
+		super();
+		color1 = _color1;
+		color2 = _color2;
+		x = _x;
+		y = _y;
+		w = _w;
+		h = _h;
+		x1 = _x1;
+		y1 = _y1;
+		x2 = _x2;
+		y2 = _y2;
+		repeat = _repeat;
+		shape = _shape;
+		
+		EGL2.gradients.add(this);
+	}
+	
+	/**
+	 * <code>public void translateAll(int dx, int dy)</code></br></br>
+	 * Moves the shape. Will not update the coordinates for the colors.
+	 * @param dx Change in x
+	 * @param dy Change in y
+	 */
+	public void translate(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	/**
+	 * <code>public void translateAll(int dx, int dy)</code></br></br>
+	 * Moves the shape. Updates the color coordinates as well as the position.
+	 * @param dx Change in x
+	 * @param dy Change in y
+	 */
+	public void translateAll(int dx, int dy) {
+		x += dx;
+		y += dy;
+		x1 += dx;
+		x2 += dx;
+		y1 += dy;
+		y2 += dy;
+	}
+	
+	/**
+	 * <code>public void setColor1(Color color)</code></br></br>
+	 * Used to change the first color of the gradient
+	 * @param color New color
+	 */
+	public void setColor1(Color color) {
+		color1 = color;
+	}
+	
+	/**
+	 * <code>public void setColor2(Color color)</code></br></br>
+	 * Used to change the second color of the gradient
+	 * @param color New color
+	 */
+	public void setColor2(Color color) {
+		color2 = color;
+	}
+	
+	/**
+	 * <code>public Color getColor1()</code></br></br>
+	 * Gets the current color of the first part of the gradient
+	 * @return current first color
+	 */
+	public Color getColor1() {
+		return color1;
+	}
+	
+	/**
+	 * <code>public Color getColor2()</code></br></br>
+	 * Gets the current color of the second part of the gradient
+	 * @return current second color
+	 */
+	public Color getColor2() {
+		return color2;
+	}
+	
+	/**
+	 * <code>public void setRepeatable(boolean repeatable)</code></br></br>
+	 * Changes whether the color gradient pattern will repeat or not
+	 * @param repeatable true if repeatable, false if not
+	 */
+	public void setRepeatable(boolean repeatable) {
+		repeat = repeatable;
+	}
+	
+	/**
+	 * <code>public boolean isRepeatable()</code></br></br>
+	 * Checks whether the gradient color pattern is repeating or not
+	 * @return true if repeating, false if not
+	 */
+	public boolean isRepeatable() {
+		return repeat;
+	}
+	
+	/**
+	 * <code>public void show()</code></br></br>
+	 * Make the shape visible
+	 */
+	public void show() {
+		visible = true;
+	}
+	
+	/**
+	 * <code>public void hide()</code></br></br>
+	 * Hide the shape
+	 */
+	public void hide() {
+		visible = false;
+	}
+	
+	/**
+	 *<code>public boolean isVisible()</code></br></br>
+	 * Checks whether the shape is visible or not
+	 * @return true if visible, false if not
+	 */
+	public boolean isVisible() {
+		return visible;
+	}
 }
 
 /**
@@ -619,14 +1030,61 @@ class DrawPanel extends JPanel {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		
+		// Change background color
+		g2.setColor(EGL2.backgroundColor);
+		g2.fillRect(0, 0, EGL2.getWindowWidth(), EGL2.getWindowHeight());
+		
+		// Rectangles
 		for (Rectangle rect : EGL2.rects) {
-			g2.setColor(rect.color);
-			g2.fillRect(rect.x, rect.y, rect.w, rect.h);
+			
+			if (rect.visible) {
+				g2.setColor(rect.color);
+
+				if (rect.filled) {
+					g2.fillRect(rect.x, rect.y, rect.w, rect.h);
+				} else {
+					g2.drawRect(rect.x, rect.y, rect.w, rect.h);
+				}
+			}
 		}
 		
+		// Circles
 		for (Circle circ : EGL2.circles) {
-			g2.setColor(circ.color);
-			g2.fillOval(circ.x, circ.y, circ.w, circ.h);
+			
+			if (circ.isVisible()) {
+				g2.setColor(circ.color);
+
+				if (circ.filled) {
+					g2.fillOval(circ.x, circ.y, circ.w, circ.h);
+				} else {
+					g2.drawOval(circ.x, circ.y, circ.w, circ.h);
+				}
+			}
+		}
+		
+		// Lines
+		for (Line line : EGL2.lines) {
+			
+			if (line.visible) {
+				g2.setColor(line.color);
+				g2.drawLine(line.x1, line.y1, line.x2, line.y2);
+			}
+		}
+		
+		// Gradients
+		for (Gradient grad : EGL2.gradients) {
+			GradientPaint gp = new GradientPaint(grad.x1, grad.y1, grad.color1,
+													grad.x2, grad.y2, grad.color2, grad.repeat);
+			if (grad.visible) {
+				g2.setPaint(gp);
+
+				if (grad.shape == "rectangle") {
+					g2.fillRect(grad.x, grad.y, grad.w, grad.h);
+				}
+				if (grad.shape == "circle") {
+					g2.fillOval(grad.x, grad.y, grad.w, grad.h);
+				}
+			}
 		}
 		
 		repaint();
--- test.java
+++ test.java
@@ -29,26 +29,41 @@ public class test {
 		//Rectangle taht = EGL2.Draw.rect(0, 0, 1000, 1000, true, Color.WHITE);
 		Rectangle stuff = EGL2.Draw.rect(100, 100, 100, 100, true, EGL2.BLUE);
 		Rectangle small = EGL2.Draw.rect(250, 200, 10, 40, true, Color.CYAN);
-		Circle thing = EGL2.Draw.circle(0, 0, 20, 20, true, EGL2.BLACK);
-		Circle another = EGL2.Draw.circle(60, 90, 40, 70, true, Color.GREEN);
+		Circle thing = EGL2.Draw.circle(0, 0, 20, 20, false, EGL2.BLACK);
+		//Circle another = EGL2.Draw.circle(60, 90, 40, 70, true, Color.GREEN);
 		int windowX = EGL2.getWindowX();
 		int windowY = EGL2.getWindowY();
+		//EGL2.setBackgroundColor(Color.DARK_GRAY);
 		EGL2.setResizable(true);
+		Line yet = EGL2.Draw.line(50, 50, 150, 200, Color.BLUE);
+		Line grow = EGL2.Draw.line(300, 550, 400, 350, Color.BLACK);
+		Line a = EGL2.Draw.line(60, 80, 90, 110, Color.MAGENTA);
+		Gradient grad = EGL2.Draw.gradient(0, 0, 100, 100, 0, 0, Color.BLUE, 100, 100, Color.RED, true, "circle");
+		Gradient grad1 = EGL2.Draw.gradient(0, 100, 100, 100, 0, 100, Color.BLUE, 100, 100, Color.RED, false, "rectangle");
 		
 		//stuff.x += 500;
 		while (true) {
 			
 			EGL2.update(60);
-			thing.x += windowSpeed;
-			//stuff.translate(rectSpeed, 0);
+			grad.translate(4, 0);
+			grad1.translate(4, 0);
+			thing.translate(windowSpeed, 0);
 			
+			if (thing.x > EGL2.getWindowWidth() / 2) {
+				thing.hide();
+			}
+			System.out.println(EGL2.getWindowWidth());
+			//stuff.translate(rectSpeed, 0);
+			//yet.x1 += 1;
+			//yet.translate(0, -1);
 			//System.out.println(stuff.color);
 			
 			if (EGL2.getKeyDown("a")) {	
 				//ConcurrentModificationException used to occur
 				EGL2.Draw.rect(50, 10, 10, 70, true, Color.YELLOW);
 				//stuff.translate(-rectSpeed, 0);
-				stuff.x += -windowSpeed;
+				//stuff.x += -windowSpeed;
+				stuff.translate(-4, 0);
 				//stuff.setColor(Color.BLACK);
 			}
 			if (EGL2.getKeyDown("d")) {
@@ -88,10 +103,14 @@ public class test {
 				EGL2.setPosition(windowX, windowY);
 				//stuff.translate(0, -rectSpeed);
 				stuff.y -= windowSpeed;
+				thing.show();
 			}
 			if (EGL2.getKeyDown("t")) {
 				small.x += 4;
-				another.x += 5;
+				//another.x += 5;
+				stuff.extrude(-1, -1);
+				grad1.hide();
+				thing.hide();
 			}
 		}
 	}
